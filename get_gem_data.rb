5467765bf7c7678278fcb2adc89a97f61d6fa6cb
Claire Parker <claire.parker87@gmail.com>
2016-01-02 23:46:56 +0000

    :u6708: JSON is output to script directory, not cron land

--- get_gem_data.rb
+++ get_gem_data.rb
@@ -8,19 +8,20 @@ def record_downloads_for(gem_name)
   page = Mechanize.new.get("https://rubygems.org/gems/#{gem_name}")
   gem_stats = page.search('.gem__downloads')
   total_stat = gem_stats[0].text
-  puts "Today's total: #{total_stat}"
   todays_date = Time.now.strftime("%d-%m-%y")
 
   filename = "#{gem_name}-data.json"
-  if File.exist?(filename)
-    prev_data = File.read(filename)
+  filepath = File.join(File.dirname(File.expand_path(__FILE__)), filename)
+
+  if File.exist?(filepath)
+    prev_data = File.read(filepath)
     hash = JSON.parse(prev_data)
   else
     hash = {}
   end
 
   hash[todays_date] = total_stat
-  File.open(filename, 'w') do |file|
+  File.open(filepath, 'w') do |file|
     file.puts JSON.pretty_generate(hash)
   end
 end
